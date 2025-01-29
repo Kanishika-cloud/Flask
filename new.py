1be0628e4d002ff87d64ffc667fac3e5abe41158
Kanishika-cloud <kanishikachaudhary@gmail.com>
2025-01-29 10:38:14 +0530

    change

---
+++ new.py
@@ -0,0 +1,27 @@
+# class A:
+#     def __init__(self, name):
+#         self.name = name
+#         print(name)
+
+#     def add(self, a, b):
+#         self.a=a
+#         self.b=b
+#         return a+b
+
+# class B(A):
+#     def sub(self, a, b):
+#      self.a=a
+#      self.b=b
+#      return a-b
+    
+# i1=B("kanishika")
+# print(i1.add(2,3))
+
+
+_map={
+    'a':2,
+    'c':4,
+    'd':5
+}
+for k,v in _map.items():
+    print(k, v)
